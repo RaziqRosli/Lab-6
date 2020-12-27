4ce98f5dceb29f1abb0483282e36ec41eeb5e7f8
RaziqRosli <73064476+RaziqRosli@users.noreply.github.com>
2020-12-27 19:39:51 +0800

    Update 6.2.client.py

--- 6.2.client.py
+++ 6.2.client.py
@@ -5,21 +5,17 @@ host = '192.168.0.200'
 port = 8888
 
 print('Waiting for connection')
-
 try:
-        ClientSocket.connect((host,port))
-
+    ClientSocket.connect((host,port))
 except socket.error as e:
-        print(str(e))
+    print(str(e))
 
 response = ClientSocket.recv(1024)
-
 print(response)
-
 while True:
-        Input = input("Say something : ")
-        ClientSocket.send(str.encode(Input))
-        response = ClientSocket.recv(1024)
-        print(response.decode('utf-8'))
+    Input = input("Say something : ")
+    ClientSocket.send(str.encode(Input))
+    response = ClientSocket.recv(1024)
+    print(response.decode('utf-8'))
 
 ClientSocket.close()
